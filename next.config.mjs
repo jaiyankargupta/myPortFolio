552b65bbc466573f8485c598315d4fbb28dfa550
jaiyankargupta <jaiyankargupta@gmail.com>
2024-07-08 12:31:44 +0530

    Step 2 : setup velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
-import withMDX from '@next/mdx';
+import { build } from 'velite'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-};
+export default {
+  // othor next config here...
+  webpack: config => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  }
+}
 
-export default withMDX(nextConfig);
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
